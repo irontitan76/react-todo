928f7c360724936944a15fbd205249795c29f358
Ross Sheppard <ross_sheppard@outlook.com>
2016-08-03 23:29:17 -0500

    Add TodoSearch tests

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -16,7 +16,7 @@ var AddTodo = React.createClass({
     return (
       <div>
         <form ref="form" onSubmit={this.handleSubmit}>
-          <input type="text" ref="todoText" placeholder="Enter new todo"/>
+          <input type="text" ref="todoText" placeholder="What do you need to do?"/>
           <button className="button expanded">Add Todo</button>
         </form>
       </div>
