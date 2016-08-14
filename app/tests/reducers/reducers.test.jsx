cedd73ac8f4501c651b94fa5053d6d41bff33aac
Ross Sheppard <ross_sheppard@outlook.com>
2016-08-14 16:38:07 -0500

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -28,4 +28,40 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo completion', () => {
+      var todos = [
+        {
+          id: 123,
+          text: 'Test todo 1',
+          completed: true,
+          createdAt: 3523325,
+          completedAt: 3523328
+        }
+      ];
+
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 123
+      };
+
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toNotExist();
+    });
+  });
 });
