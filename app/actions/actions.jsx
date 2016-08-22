198a7bfef1f4fae1a5c8d99ec10d104908d6f422
Ross Sheppard <ross_sheppard@outlook.com>
2016-08-21 23:10:26 -0500

    Add actions for firebase login and logout

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -1,4 +1,4 @@
-import firebase, {firebaseRef} from 'app/firebase/';
+import firebase, {firebaseRef, githubProvider} from 'app/firebase/';
 import moment from 'moment';
 
 export var setSearchText = (searchText) => {
@@ -86,6 +86,24 @@ export var startToggleTodo = (id, completed) => {
 
     return todoRef.update(updates).then(() => {
       dispatch(updateTodo(id, updates));
+    });
+  };
+};
+
+export var startLogin = () => {
+  return (dispatch, getState) => {
+    return firebase.auth().signInWithPopup(githubProvider).then((result) => {
+      console.log('Authorization successful!', result);
+    }, (e) => {
+      console.log('Unable to authorize', e);
+    });
+  };
+};
+
+export var startLogout = () => {
+  return (dispatch, getState) => {
+    return firebase.auth().signOut().then(() => {
+      console.log('Logged out');
     });
   };
 };
--- app/app.jsx
+++ app/app.jsx
@@ -3,11 +3,11 @@ var ReactDOM = require('react-dom');
 var { Provider } = require('react-redux');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-var TodoApp = require('TodoApp');
-
 var actions = require('actions');
 var store = require('configureStore').configure();
 var TodoAPI = require('TodoAPI');
+import Login from 'Login';
+import TodoApp from 'TodoApp';
 
 // LOCAL STORAGE
 // store.subscribe(() => {
@@ -29,7 +29,12 @@ require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
   <Provider store={ store }>
-    <TodoApp/>
+    <Router history={hashHistory}>
+      <Route path="/">
+        <IndexRoute component={Login}/>
+        <Route path="/todos" component={TodoApp}></Route>
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
